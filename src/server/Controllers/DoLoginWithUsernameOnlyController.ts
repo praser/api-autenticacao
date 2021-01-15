e6e048b48d194fb9cb447667ed336b28389cdcb0
Rubens Praser Júnior <praser@gmail.com>
2021-01-15 11:07:41 -0300

    Remoção da funcionalidade de autenticação sem credenciais devido a inviabilidade técnica de lidar com o windows authentication

--- src/server/Controllers/DoLoginWithUsernameOnlyController.ts
+++ src/server/Controllers/DoLoginWithUsernameOnlyController.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from 'express';
 
-import fetch from 'node-fetch';
-
 import HttpStatusCode from '../httpStatusCode';
 import DoLoginWithUsernameOnlyUseCase from '../../useCases/DoLoginWithUsernameOnly/DoLoginWithUsernameOnlyUseCase';
 
@@ -12,32 +10,13 @@ class DoLoginWithUsernameOnlyController {
     this.doLoginWithUsernameOnlyUseCase = doLoginWithUsernameOnlyUseCase;
   }
 
-  private static isWithCredentials(request: Request): boolean {
-    return 'credentials' in request.body;
-  }
-
   private static getUsernameFromRequest(request: Request): string {
     return request.body.credentials.username as string;
   }
 
-  private static async getUsernameFromOS(): Promise<string> {
-    const res = await fetch(process.env.SESSION_USER_ENDPOINT as string);
-    const json = await res.json();
-    const { id: username } = json;
-
-    return new Promise((resolve, reject) => (username ? resolve(username) : reject()));
-  }
-
   async handle(request: Request, response: Response): Promise<Response> {
     try {
-      let username: string;
-
-      if (DoLoginWithUsernameOnlyController.isWithCredentials(request)) {
-        username = DoLoginWithUsernameOnlyController.getUsernameFromRequest(request);
-      } else {
-        username = await DoLoginWithUsernameOnlyController.getUsernameFromOS();
-      }
-
+      const username = DoLoginWithUsernameOnlyController.getUsernameFromRequest(request);
       const body = await this.doLoginWithUsernameOnlyUseCase.execute(username);
       return response.status(HttpStatusCode.OK).json(body);
     } catch (error) {
